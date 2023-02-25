9fdfd0c510e70ca22efad70a6719e8160a02a946
s1veme <kpdpav@gmail.com>
2023-02-25 15:18:29 +0300

    update(app/api/dependencies): add security_settings in DependentService

--- app/api/dependencies/service.py
+++ app/api/dependencies/service.py
@@ -4,14 +4,26 @@ from databases import Database
 from fastapi import Depends
 
 from app.api.dependencies.database import _get_db_pool
+from app.api.dependencies.security import get_security_settings
+from app.core.settings.security import SecuritySettings
 from app.repositories.base import BaseRepository
 from app.services.base import BaseService
 
 
 class DependentService:
-    def __init__(self, service: Type[BaseService], repository: Type[BaseRepository]) -> None:
+    def __init__(
+        self, service: Type[BaseService], repository: Type[BaseRepository], is_security_settings: bool = False
+    ) -> None:
         self.repository = repository
         self.service = service
+        self.is_security_settings = is_security_settings
 
-    def __call__(self, database: Database = Depends(_get_db_pool)) -> BaseService:
-        return self.service(self.repository(database))
+    # TODO: review
+    def __call__(
+        self,
+        database: Database = Depends(_get_db_pool),
+        security_settings: SecuritySettings = Depends(get_security_settings),
+    ) -> BaseService:
+        if not self.is_security_settings:
+            return self.service(self.repository(database))
+        return self.service(self.repository(database), security_settings)
