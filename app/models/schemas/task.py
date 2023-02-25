8e4785fcbbb6c99fb0c1e476881f4e2e603c62f5
s1veme <kpdpav@gmail.com>
2023-02-25 15:26:12 +0300

    update(app/models/schemas): add status in TaskOut, TaskIn, delete UpdateTask

--- app/models/schemas/task.py
+++ app/models/schemas/task.py
@@ -5,12 +5,10 @@ class TaskOut(BaseModel):
     id: int
     title: str
     content: str
+    status: str
 
 
 class TaskIn(BaseModel):
     title: str
     content: str
-
-
-class UpdateTask(BaseModel):
-    ...
+    status: str
