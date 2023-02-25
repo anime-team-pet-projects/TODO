59609c8185df719983ab4a8f48c8019f0c1ad5bd
s1veme <kpdpav@gmail.com>
2023-02-25 15:15:15 +0300

    add(app/core): add get_security_settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,14 @@
-
 from app.core.settings.app import AppSettings
+from app.core.settings.security import SecuritySettings
 
 
 def get_app_settings() -> AppSettings:
     return AppSettings()
+
+
+def get_security_settings(secret_key: str, algorithm: str, access_token_expire_minutes: int):
+    return SecuritySettings(
+        secret_key=secret_key,
+        algorithm=algorithm,
+        access_token_expire_minutes=access_token_expire_minutes,
+    )
--- app/core/events.py
+++ app/core/events.py
@@ -3,6 +3,7 @@ from typing import Callable
 from fastapi import FastAPI
 from loguru import logger
 
+from app.core.config import get_security_settings
 from app.core.settings.app import AppSettings
 from app.db.events import close_db_connection, connect_to_db
 
@@ -13,6 +14,12 @@ def create_start_app_handler(
 ) -> Callable:
     async def start_app() -> None:
         await connect_to_db(app, settings)
+        security_settings = get_security_settings(
+            secret_key=settings.secret_key,
+            algorithm=settings.algorithm,
+            access_token_expire_minutes=settings.access_token_expire_minutes,
+        )
+        app.state.security_settings = security_settings
 
     return start_app
 
